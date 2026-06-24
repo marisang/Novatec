7c26ea76d015daced0b8cb05e4cf21e6e7559946
Marisang <mari.sangean@gmail.com>
2026-06-24 11:10:58 -0300

    Arrumar views.py

--- config/chat/views.py
+++ config/chat/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from chat.models import Projeto
 
 def home(request):
     return render(request, 'chat.html')
 
 def chat(request):
-    body = json.loads(
-        request.body
+    projetos = Projeto.objects.filter(
+        status="andamento"
     )
-    mensagem = body["mensagem"]
+    nomes = [
+        projeto.nome
+        for projeto in projetos
+    ]
     return JsonResponse({
-        "resposta":
-        f"Você disse: {mensagem}"
+        "resposta": str(nomes)
     })
