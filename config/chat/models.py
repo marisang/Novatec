46841bcc7abe80580a9259799adb20105d3b1712
Marisang <mari.sangean@gmail.com>
2026-06-24 11:08:30 -0300

    Arrumar models.py

--- config/chat/models.py
+++ config/chat/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=255)
     segmento = models.CharField(max_length=100)
-    data_entrada = models.DateField()
+    data_entrada = models.DateField(null=True, blank=True)
+    @property  #faz com que o valor total seja uma soma automática dos valores de todos os preojetos do cliente, ***não consegui testar
+    def valor_total_cliente(self):
+        return self.projetos.aggregate(
+            total=Sum("valor_projeto")
+        )["total"] or 0
+
     def __str__(self):
         return self.nome
 
@@ -13,51 +19,11 @@ class Projeto(models.Model):
         on_delete=models.CASCADE,
         related_name="projetos"
     )
-    nome = models.CharField(max_length=255)
-    status = models.CharField(max_length=50)
-    valor = models.DecimalField(
+    nome_projeto = models.CharField(max_length=255)
+    status_projeto = models.CharField(max_length=50)
+    valor_total_projeto= models.DecimalField(
         max_digits=10,
         decimal_places=2
-    )
-    data_inicio = models.DateField()
-    data_fim = models.DateField(
-        null=True,
-        blank=True
     )
     def __str__(self):
-        return self.nome
-
-class Pagamento(models.Model):
-    projeto = models.ForeignKey(
-        Projeto,
-        on_delete=models.CASCADE,
-        related_name="pagamentos"
-    )
-    valor = models.DecimalField(
-        max_digits=10,
-        decimal_places=2
-    )
-    data_pagamento = models.DateField()
-    status = models.CharField(
-        max_length=50
-    )
-
-class Contrato(models.Model):
-    cliente = models.ForeignKey(
-        Cliente,
-        on_delete=models.CASCADE,
-        related_name="contratos"
-    )
-    arquivo_pdf = models.TextField()
-    data_assinatura = models.DateField()
-    class ChatHistory(models.Model):
-        session_id = models.CharField(
-        max_length=255
-    )
-    role = models.CharField(
-        max_length=20
-    )
-    content = models.TextField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+        return self.nome_projeto
